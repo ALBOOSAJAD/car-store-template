5ce0884e3a1b4b3015fe3bcb3808667eb78a057f
ALBOOSAJAD <ALBOOSAJAD@gmail.com>
2022-01-13 21:40:14 +0330

    created bran-box such as facebook,linkdin..

--- sajad.js
+++ sajad.js
@@ -1,7 +1,17 @@
-let firstname="sajad";
-let lastname="alboo"
-let age="34"
-const email="sajadalboo@gmail.com"
-const identification="1819870464"
+// let firstname="sajad";
+// let lastname="alboo"
+// let age="34"
+// const email="sajadalboo@gmail.com"
+// const identification="1819870464"
+
+// console.log(firstname);
+// console.log(identification);
 
 
+
+function avrage(x,y,z){
+
+    return x+y+z/3
+}
+
+console.log (avrage(3,3,3));
