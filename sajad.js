eb167406af210fc6cf616ad251f03a99ead687a2
ALBOOSAJAD <ALBOOSAJAD@gmail.com>
2022-01-14 18:28:22 +0330

    end footer

--- sajad.js
+++ sajad.js
@@ -9,9 +9,45 @@
 
 
 
-function avrage(x,y,z){
+// function sum(x,y) {
+//     return x+y;
+// }
+// console.log(sum(3,3));
 
-    return x+y+z/3
+// function avrage(x,y,z){
+//     return x+y+z/3
+// }
+// console.log(avrage(3,4,3));
+
+
+// function sum(r,t){
+//     return r+t
+// }
+// console.log(sum(3,4));
+
+// function menha(c,b){
+//     return c-b
+// }
+// console.log(menha(3,5));
+
+
+// function sayhello(){
+//     console.log("hello");
+// }
+// sayhello()
+
+// let fname="SAJAD"
+// let lname= "ALBOGHOBEISH"
+
+// function fullname(fname,lname){
+//     return fname+ " " + lname
+// }
+// console.log(fullname(fname,lname));
+
+let firstname="sajad"
+let lastname="alboo"
+
+function welcome(firstname,lastname){
+    console.log("welcome" +" "+firstname+lastname);
 }
-
-console.log (avrage(3,3,3));
+welcome(firstname,lastname)
